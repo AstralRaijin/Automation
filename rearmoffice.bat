7083537b9f390fe33dfba83d6b13fe3220acebef
Thanh Tran <astralraijin@gmail.com>
2022-01-17 22:26:02 +0700

    update automation materials

---
+++ rearmoffice.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo Rearming your Office for trial use...
+start "" "C:\Program Files\Microsoft Office\Office16\OSPPREARM.EXE"
+echo Your Office trial has been extend for One Week.
+
+exit
